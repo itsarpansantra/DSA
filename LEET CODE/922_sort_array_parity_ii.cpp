5c6c53fe64032e7eaea1c8ca36cd5a1805233f56
itsarpansantra <144213509+itsarpansantra@users.noreply.github.com>
2025-12-23 20:26:36 +0530

    Add files via upload

---
+++ LEET CODE/922_sort_array_parity_ii.cpp
@@ -0,0 +1,45 @@
+/*
+Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
+
+Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even.
+
+Return any answer array that satisfies this condition.
+
+ 
+
+Example 1:
+
+Input: nums = [4,2,5,7]
+Output: [4,5,2,7]
+Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+Example 2:
+
+Input: nums = [2,3]
+Output: [2,3]
+*/
+# include<iostream>
+# include<vector>
+using namespace std;
+vector<int> parity(vector<int>&v){
+    int n = v.size();
+    if(n<=2) return v;
+    int i=0,j=1;
+
+    while(i<n&&j<n){
+        if(v[i]%2==0) i+=2;
+        else if(v[j]%2==1) j+=2;
+        else{
+            swap(v[i],v[j]);
+            i+=2;
+            j+=2;
+        }
+    }
+    return v;
+}
+int main(){
+    vector<int>v = {4,2,5,7};
+    vector<int>s = parity(v);
+    for(int i=0;i<s.size();i++){
+        cout<<s[i]<<" ";
+    }
+}
