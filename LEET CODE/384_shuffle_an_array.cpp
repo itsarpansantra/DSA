5383e47729edadd1f7fddccc86a1782be1bb96ff
itsarpansantra <144213509+itsarpansantra@users.noreply.github.com>
2026-04-06 10:26:22 +0530

    Add files via upload

---
+++ LEET CODE/384_shuffle_an_array.cpp
@@ -0,0 +1,80 @@
+/*
+Given an integer array nums, design an algorithm to randomly shuffle the array. All permutations of the array should be equally likely as a result of the shuffling.
+
+Implement the Solution class:
+
+Solution(int[] nums) Initializes the object with the integer array nums.
+int[] reset() Resets the array to its original configuration and returns it.
+int[] shuffle() Returns a random shuffling of the array.
+ 
+
+Example 1:
+
+Input
+["Solution", "shuffle", "reset", "shuffle"]
+[[[1, 2, 3]], [], [], []]
+Output
+[null, [3, 1, 2], [1, 2, 3], [1, 3, 2]]
+
+Explanation
+Solution solution = new Solution([1, 2, 3]);
+solution.shuffle();    // Shuffle the array [1,2,3] and return its result.
+                       // Any permutation of [1,2,3] must be equally likely to be returned.
+                       // Example: return [3, 1, 2]
+solution.reset();      // Resets the array back to its original configuration [1,2,3]. Return [1, 2, 3]
+solution.shuffle();    // Returns the random shuffling of array [1,2,3]. Example: return [1, 3, 2]
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 50
+-106 <= nums[i] <= 106
+All the elements of nums are unique.
+At most 104 calls in total will be made to reset and shuffle.
+*/
+# include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    private:
+    vector<int>original;
+    vector<int>ans;
+    public:
+    Solution(vector<int>&num){
+        original=num;
+        ans = num;
+    }
+    vector<int>reset(){
+        ans = original;
+        return ans;
+    }
+
+    vector<int>shuffle(){
+        int n = ans.size();
+        for(int i=0;i<n;i++){
+            int j = i+rand()%(n-i);
+            swap(ans[i],ans[j]);
+        }
+        return ans;
+    }
+};
+int main(){
+    vector<int>num = {1,2,3};
+    Solution solution(num);
+
+    // shuffle1 
+    vector<int>shuffle1 = solution.shuffle();
+    for(int x: shuffle1) cout<<x<<" ";
+    cout<<endl;
+
+    // reset
+    vector<int>reset1 = solution.reset();
+    for(int x: reset1) cout<<x<<" ";
+    cout<<endl;
+
+
+    // shuffle2 
+    vector<int>shuffle2 = solution.shuffle();
+    for(int x: shuffle2) cout<<x<<" ";
+    cout<<endl;
+}
