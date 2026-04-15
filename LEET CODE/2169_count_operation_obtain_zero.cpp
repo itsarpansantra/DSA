863a43be17e6796614aba1e82f208af32a62bfdd
itsarpansantra <144213509+itsarpansantra@users.noreply.github.com>
2026-04-15 22:02:16 +0530

    Add files via upload

---
+++ LEET CODE/2169_count_operation_obtain_zero.cpp
@@ -0,0 +1,53 @@
+/*
+You are given two non-negative integers num1 and num2.
+
+In one operation, if num1 >= num2, you must subtract num2 from num1, otherwise subtract num1 from num2.
+
+For example, if num1 = 5 and num2 = 4, subtract num2 from num1, thus obtaining num1 = 1 and num2 = 4. However, if num1 = 4 and num2 = 5, after one operation, num1 = 4 and num2 = 1.
+Return the number of operations required to make either num1 = 0 or num2 = 0.
+
+ 
+
+Example 1:
+
+Input: num1 = 2, num2 = 3
+Output: 3
+Explanation: 
+- Operation 1: num1 = 2, num2 = 3. Since num1 < num2, we subtract num1 from num2 and get num1 = 2, num2 = 3 - 2 = 1.
+- Operation 2: num1 = 2, num2 = 1. Since num1 > num2, we subtract num2 from num1.
+- Operation 3: num1 = 1, num2 = 1. Since num1 == num2, we subtract num2 from num1.
+Now num1 = 0 and num2 = 1. Since num1 == 0, we do not need to perform any further operations.
+So the total number of operations required is 3.
+*/
+# include<iostream>
+using namespace std;
+int operation(int num1,int num2){
+    int count =0;
+    // while(num1>0 && num2>0){
+    //     if(num1>=num2) num1-=num2;
+    //     else num2 -=num1;
+    //     count++;
+    // }
+
+    // another approach // let a = 15, b = 4 . if we 15-4 =11-4.. so 15/4 = 3 
+    while(num1>0 && num2>0){
+        if(num1>=num2){
+            count+= (num1/num2); // count += 15/4 = 3
+            num1 = num1%num2; // and ata num1 = 15%4 = 3 
+            // next roud count =3 , ar num1 = 3 , num2 = 4 
+        }
+        else{
+            count+= (num2/num1);
+            num2 = num2%num1;
+        }
+    }
+    return count;
+}
+int main(){
+    int num1,num2;
+    cout<<"Enter num1 value : ";
+    cin>>num1;
+    cout<<"Enter num2 value : ";
+    cin>>num2;
+    cout<<operation(num1,num2);
+}
